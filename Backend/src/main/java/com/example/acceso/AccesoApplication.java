5c2e3bcfb1673925b11dba4120626ff440a919cf
JohnCh-867 <johnandersonchaponanm@gmail.com>
2026-07-19 22:18:46 -0500

    prueba

--- Backend/src/main/java/com/example/acceso/AccesoApplication.java
+++ Backend/src/main/java/com/example/acceso/AccesoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AccesoApplication {
 	public static void main(String[] args) {
+		// Prueba1
 		SpringApplication.run(AccesoApplication.class, args);
 	}
 }
